3bee0f3214d8266cb5be84e95b83259dde31f1ad
yotiky <yotiky@gmail.com>
2020-06-29 10:12:27 +0900

    feat: basic usage samples.

---
+++ Assets/Scripts/Sample.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+using System;
+using System.Runtime.CompilerServices;
+
+public class Sample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    async void Start()
+    {
+        await BasicUsageSampleAsync();
+    }
+
+    #region Basic usage
+    private async UniTask BasicUsageSampleAsync()
+    {
+        using(var meas = new Measurement(nameof(BasicUsageUniTask)))
+        {
+            await BasicUsageUniTask();
+        }
+
+        using (var meas = new Measurement(nameof(BasicUsageUniTaskNotAwait)))
+        {
+            await BasicUsageUniTaskNotAwait();
+        }
+
+        bool result;
+        using (var meas = new Measurement(nameof(BasicUsageUniTaskT)))
+        {
+            result = await BasicUsageUniTaskT();
+        }
+        Debug.Log("BasicUsageUniTaskT Result:" + result);
+
+        using (var meas = new Measurement(nameof(BasicUsageUniTaskVoid)))
+        {
+            BasicUsageUniTaskVoid().Forget();
+        }
+    }
+    // 戻り値を持たない非同期処理
+    private async UniTask BasicUsageUniTask()
+    {
+        await UniTask.Delay(1000);
+    }
+    // 待つ必要がなければ await せずに UniTask を返す (コンパイルで await の展開が入ら無いので余計なオーバーヘッドを防げる)
+    private UniTask BasicUsageUniTaskNotAwait()
+    {
+        return UniTask.Delay(1000);
+    }
+    // 戻り値を持つ非同期処理
+    private async UniTask<bool> BasicUsageUniTaskT()
+    {
+        await UniTask.Delay(1000);
+        return true;
+    }
+    // async void 相当、Forget() とセットで使う
+    private async UniTaskVoid BasicUsageUniTaskVoid()
+    {
+        await UniTask.Delay(1000);
+    }
+    #endregion
+
+}
---
+++ Assets/Scripts/ValueStopwatch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public readonly struct ValueStopwatch
+{
+    static readonly double TimestampToTicks = TimeSpan.TicksPerSecond / (double)Stopwatch.Frequency;
+
+    readonly long startTimestamp;
+
+    public static ValueStopwatch StartNew() => new ValueStopwatch(Stopwatch.GetTimestamp());
+
+    ValueStopwatch(long startTimestamp)
+    {
+        this.startTimestamp = startTimestamp;
+    }
+
+    public TimeSpan Elapsed => TimeSpan.FromTicks(this.ElapsedTicks);
+
+    public long ElapsedTicks
+    {
+        get
+        {
+            if (startTimestamp == 0)
+            {
+                throw new InvalidOperationException("Detected invalid initialization(use 'default'), only to create from StartNew().");
+            }
+
+            var delta = Stopwatch.GetTimestamp() - startTimestamp;
+            return (long)(delta * TimestampToTicks);
+        }
+    }
+}
+
+public static class ValueStopwatchExtentions
+{
+    public static void Restart(this ref ValueStopwatch sp)
+    {
+        sp = ValueStopwatch.StartNew();
+    }
+}
+
+public class Measurement : IDisposable
+{
+    private string key;
+    private ValueStopwatch sw = ValueStopwatch.StartNew();
+    public Measurement(string key)
+    {
+        this.key = key;
+    }
+    public void Dispose()
+    {
+        UnityEngine.Debug.Log($"{key}: {sw.Elapsed.TotalSeconds}s");
+    }
+}
